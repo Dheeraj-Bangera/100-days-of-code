cb7a70310a87b0f56ae2848e5aafb0a5b088e635
Dheeraj-Bangera <114220695+Dheeraj-Bangera@users.noreply.github.com>
2023-06-22 23:10:38 +0530

    Create day3.cpp

---
+++ day3.cpp
@@ -0,0 +1,22 @@
+//714. Best Time to Buy and Sell Stock with Transaction Fee
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+     int n = prices.size();
+        int ans = 0;
+        int mn = prices[0];
+        for(int i=0;i<n;i++)
+        {
+         if (prices[i] < mn)
+                mn = prices[i];
+         if(prices[i] > mn + fee)
+         {
+              ans += prices[i] - fee - mn;
+              mn = prices[i] - fee;
+         }
+            
+        }
+           return ans;  
+        }
+   
+};
