df3b18c85ef28d437b0e1f1223d4d8a298a533d6
Dheeraj-Bangera <114220695+Dheeraj-Bangera@users.noreply.github.com>
2023-07-10 22:06:48 +0530

    Create day21.cpp

---
+++ day21.cpp
@@ -0,0 +1,17 @@
+//111. Minimum Depth of Binary Tree
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+
+        if(root->left && root->right){
+            return min(left+1, right+1);
+        }
+        return max(left+1, right+1);
+    }
+};
