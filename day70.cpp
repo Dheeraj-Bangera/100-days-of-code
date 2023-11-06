b3fcc86803ae6ff0d926a79d537375419ed85f5a
Dheeraj Bangera <114220695+Dheeraj-Bangera@users.noreply.github.com>
2023-11-07 00:23:44 +0530

    Create day70.cpp

---
+++ day70.cpp
@@ -0,0 +1,54 @@
+//2483. Minimum Penalty for a Shop
+class Solution {
+
+public:
+
+    int bestClosingTime(string custom) {
+
+        int y=0,n=0;
+
+        for(auto i:custom){
+
+            if(i=='Y')y++;
+
+        }
+
+        int mn = INT_MAX,index,penalty;
+
+        for(int i=0;i<custom.size();i++){
+
+            penalty = y + n;
+
+            if(penalty<mn){
+
+                mn = penalty;
+
+                index = i;
+
+            }
+
+            if(custom[i]=='Y')y--;
+
+            else n++;
+
+        }
+
+        penalty = y + n;
+
+        if(penalty<mn){
+
+                mn = penalty;
+
+                index = custom.size();
+
+        }
+
+        return index;
+
+    }
+
+};
+
+
+
+    
